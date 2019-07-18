3c191fa5261ff3fa25c912c43a59ba854ab20635
Barron Wei <barronwei@outlook.com>
2019-07-18 10:34:26 +0200

    engine class

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,78 @@
-import Debug from 'debug'
+import * as PayPal from 'paypal-rest-sdk'
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
+import * as bodyParser from 'koa-bodyparser'
 import { Redis } from 'ioredis'
 import { Server } from 'net'
-
 import { Account } from './models/account'
 
-export interface EngineConfig {
+const DEFAULT_PORT = 3000
+const DEFAULT_PREFIX = ''
+const DEFAULT_MIN_DROPS = 10000
+
+export interface PayPalEngineConfig {
+  connectorUrl: string
+  port?: number
+
   address: string
-  assetScale: string
-  redis: Redis
   secret: string
 
-  port?: number
+  redis: Redis
+
+  assetScale: number
+  prefix?: string
+
+  ppClient: PayPal.Payment
+  ppUri: string
+  minDrops?: number
 }
 
-export class SettlementEngine {
+export class PayPalSettlementEngine {
   app: Koa
+  connectorUrl: string
   port: number
+
+  address: string
+  secret: string
+
   redis: Redis
+
+  assetScale: number
+  prefix: string
+
+  ppClient: PayPal.Payment
+  minDrops: number
+
   router: Router
   server: Server
 
-  constructor (config: EngineConfig) {
+  constructor (config: PayPalEngineConfig) {
     this.app = new Koa()
+    this.app.use(async (ctxt, next) => {
+      if (ctxt.path.includes('messages')) ctxt.disableBodyParser = true
+      await next()
+    })
+    this.app.use(bodyParser())
+
+    this.port = config.port || DEFAULT_PORT
+    this.connectorUrl = config.connectorUrl
+
+    this.address = config.address
+    this.secret = config.secret
 
     this.redis = config.redis
+
+    this.assetScale = config.assetScale
+    this.prefix = config.prefix || DEFAULT_PREFIX
+
+    this.ppClient = config.ppClient
+    this.minDrops = config.minDrops || DEFAULT_MIN_DROPS
+
+    this.app.context.redis = this.redis
+    this.app.context.prefix = this.prefix
+    this.app.context.address = this.address
+
+    this.router = new Router()
   }
 
   public async start () {
