5a763f3a59426a5d32965214e7191f06203082cf
Barron Wei <barronwei@outlook.com>
2019-07-19 11:18:17 +0200

    router

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,25 @@ import * as PayPal from 'paypal-rest-sdk'
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
 import * as bodyParser from 'koa-bodyparser'
+import axios from 'axios'
 import { Redis } from 'ioredis'
 import { Server } from 'net'
 import { Account } from './models/account'
+import { v4 as uuidv4 } from 'uuid'
+
+import {
+  create as createAccount,
+  search as searchAccount,
+  remove as removeAccount
+} from './controllers/account'
+import { create as createMessage } from './controllers/message'
+import { create as createSettlement } from './controllers/settlement'
+
+// PayPal SDK mode
+const CURRENT_MODE = 'sandbox'
 
 const DEFAULT_PORT = 3000
-const DEFAULT_PREFIX = ''
+const DEFAULT_PREFIX = 'pp'
 const DEFAULT_MIN_DROPS = 10000
 
 export interface PayPalEngineConfig {
@@ -23,7 +36,6 @@ export interface PayPalEngineConfig {
   redis: Redis
 
   ppClient: PayPal.Payment
-  ppUri: string
   minDrops?: number
 }
 
@@ -72,7 +84,19 @@ export class PayPalSettlementEngine {
     this.app.context.prefix = this.prefix
     this.app.context.address = this.address
 
+    // Routes
+
     this.router = new Router()
+    this.setupRoutes()
+    this.app.use(this.router.routes())
+
+    // PayPal
+
+    PayPal.configure({
+      mode: CURRENT_MODE,
+      client_id: this.address,
+      client_secret: this.secret
+    })
   }
 
   public async start () {
@@ -83,5 +107,34 @@ export class PayPalSettlementEngine {
   public async close () {
     console.log('Shutting down')
     this.server.close()
+  }
+
+  async findAccountMiddleware (ctxt: Koa.Context, next: () => Promise<any>) {
+    const account = await ctxt.redis.get(
+      `${ctxt.prefix}:accounts:${ctxt.params.id}`
+    )
+    account ? (ctxt.account = JSON.parse(account)) : ctxt.throw(404)
+    await next()
+  }
+
+  private setupRoutes () {
+    // Accounts
+    this.router.post('/accounts', ctxt => createAccount(ctxt))
+    this.router.get('/accounts/:id', ctxt => queryAccount(ctxt))
+    this.router.delete('/accounts/:id', ctxt => removeAccount(ctxt))
+
+    // Messages
+    this.router.post(
+      '/accounts/:id/messages',
+      this.findAccountMiddleware,
+      createMessage
+    )
+
+    // Settlement
+    this.router.post(
+      '/accounts/:id/settlement',
+      this.findAccountMiddleware,
+      createSettlement
+    )
   }
 }
