1e893251f99fb7910bdc535e03d039e0a5f28ef3
Barron Wei <barronwei@outlook.com>
2019-07-19 13:25:01 +0200

    context destruction

--- src/controllers/settlement.ts
+++ src/controllers/settlement.ts
@@ -2,13 +2,12 @@ import { Context } from 'koa'
 import { normalizeAsset } from '../utils/normalizeAsset'
 
 export async function create (ctxt: Context) {
-  const accJSON = await ctxt.redis.get(
-    `${ctxt.prefix}:accounts:${ctxt.params.id}`
-  )
+  const { assetScale, params, prefix, redis, request } = ctxt
+  const accJSON = await redis.get(`${prefix}:accounts:${params.id}`)
   const account = JSON.parse(accJSON)
 
-  const body = ctxt.request.body
-  const amnt = normalizeAsset(body.scale, ctxt.assetScale, BigInt(body.amnt))
+  const body = request.body
+  const amnt = normalizeAsset(body.scale, assetScale, BigInt(body.amnt))
   await ctxt.settleAccount(account, amnt.toString())
 
   ctxt.status = 200
