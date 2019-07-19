36ac21bbcff14b62a4b78485c0c95683c795984c
Barron Wei <barronwei@outlook.com>
2019-07-19 15:27:35 +0200

    context renaming

--- src/controllers/settlement.ts
+++ src/controllers/settlement.ts
@@ -1,14 +1,14 @@
 import { Context } from 'koa'
 import { normalizeAsset } from '../utils/normalizeAsset'
 
-export async function create (ctxt: Context) {
-  const { assetScale, params, prefix, redis, request } = ctxt
+export async function create (ctx: Context) {
+  const { assetScale, params, prefix, redis, request } = ctx
   const accJSON = await redis.get(`${prefix}:accounts:${params.id}`)
   const account = JSON.parse(accJSON)
 
   const body = request.body
   const amnt = normalizeAsset(body.scale, assetScale, BigInt(body.amnt))
-  await ctxt.settleAccount(account, amnt.toString())
+  await ctx.settleAccount(account, amnt.toString())
 
-  ctxt.status = 200
+  ctx.status = 200
 }
