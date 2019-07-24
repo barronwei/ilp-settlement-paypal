49992667cb1db97ebc2eb9da9a4ad316c3cf6775
Barron Wei <barronwei@outlook.com>
2019-07-24 15:19:53 +0200

    tag logic

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,9 +1,15 @@
 import * as getRawBody from 'raw-body'
 import { Context } from 'koa'
+import { randomBytes } from 'crypto'
 
 export interface Message {
   type: string
   data: any
+}
+
+export interface PaymentDetails {
+  ppEmail: string
+  tag: number
 }
 
 export async function create (ctx: Context) {
@@ -17,9 +23,21 @@ export async function create (ctx: Context) {
 
 async function handleMessage (message: Message, ctx: Context) {
   const { type } = message
+  const { params, prefix, redis, ppEmail } = ctx
+  const accountId: string = params.id
   switch (type) {
     case 'paymentDetails':
-      return Buffer.from(ctx.ppEmail)
+      const res = await redis.get(`${prefix}:accountId:${accountId}:tag`)
+      const tag: number = res || randomBytes(4).readUInt32BE(0)
+      if (!res) {
+        await redis.set(`${prefix}:tag:${tag}:accountId`, accountId)
+        await redis.set(`${prefix}:accountId:${accountId}:tag`, tag)
+      }
+      const paymentDetails: PaymentDetails = {
+        ppEmail,
+        tag
+      }
+      return Buffer.from(JSON.stringify(paymentDetails))
     default:
       throw new Error(`This message type ${type} is unknown.`)
   }
